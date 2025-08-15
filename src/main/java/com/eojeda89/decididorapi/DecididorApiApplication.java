8c811f84268bff058adda6ed54cc6579b98ff9ae
eojeda89 <eojeda@gmx.es>
2025-08-15 14:06:07 +0200

    Initial commit

---
+++ src/main/java/com/eojeda89/decididorapi/DecididorApiApplication.java
@@ -0,0 +1,13 @@
+package com.eojeda89.decididorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecididorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecididorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eojeda89/decididorapi/DecididorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eojeda89.decididorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecididorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
